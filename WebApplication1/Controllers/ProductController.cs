bf701d42166e2ebbe978eca56152bb7331550dcc
bao.le <bao.le@CPU28-HANLUU>
2020-10-08 09:04:03 +0700

    Add project files.

---
+++ WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly EShopDB_DNCEntities eShopDB_DNC = new EShopDB_DNCEntities();
+        public ProductController()
+        {
+
+        }
+        
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
