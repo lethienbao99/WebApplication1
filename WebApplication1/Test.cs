519b3dc798384e25175d1c1f34aea9dc452f0f6d
bao.le <bao.le@CPU28-HANLUU>
2020-10-08 09:21:30 +0700

    dsdsdad

--- WebApplication1/Test.cs
+++ WebApplication1/Test.cs
@@ -7,6 +7,21 @@ namespace WebApplication1
 {
     public class Test
     {
+        public Test()
+        {
 
+        }
+        public Test(int x)
+        {
+
+        }
+        public Test(int x, int y)
+        {
+
+        }
+        public Test(int x, int y, int z)
+        {
+
+        }
     }
 }
